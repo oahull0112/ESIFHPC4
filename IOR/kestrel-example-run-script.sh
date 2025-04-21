8a93f4f871dd281b3abe4a89ed1971cb49450bb9
Olivia Hull <39199786+oahull0112@users.noreply.github.com>
2025-04-21 13:16:05 -0600

    Update IOR with specific test cases in kestrel-example-run-script.sh

--- IOR/kestrel-example-run-script.sh
+++ IOR/kestrel-example-run-script.sh
@@ -1,5 +1,51 @@
 #!/bin/bash
+#SBATCH --job-name=IORtest
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=100
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
 
-iordir=/projects/esifapps/ohull/ior-hpc4/ior/src
+export SCRATCH=/scratch/$USER/${SLURM_JOB_NAME:?}
+if [ -d $SCRATCH ]
+then
+   rm -rf $SCRATCH
+fi
+mkdir $SCRATCH; cd $SCRATCH
 
-srun -n 2 $iordir/ior -v -a POSIX -g -w -r -e -C -F -b 40G -t 1G -s 1
+ior=/path/to/ior/executable
+
+# Test 1 - POSIX Streaming, 10 GB single-segment, file-per-process
+# Small 100 MB 10-rank test
+#srun --ntasks=10 --cpu-bind=rank,verbose --distribution=block ior -v -a POSIX -i1 -g -w -r -e -C -F -b 10m -t 1m -s 1
+echo "Test 1 - POSIX Streaming, 10 GB single-segment, file-per-process"
+srun --ntasks=100 --cpu-bind=rank --distribution=block $ior -v -a POSIX -i5 -g -w -r -e -C -F -b 100m -t 1m -s 1
+
+# Test 2 - MPI Streaming, 100 GB single-segment, single file
+# Small 100 MB 10-rank test
+#srun --ntasks=10 --cpu-bind=rank,verbose --distribution=block ior -v -a MPIIO -i1 -g -w -r -e -C -b 10m -t 1m -s 1
+echo "Test 2 - MPI Streaming, 100 GB single-segment, single file"
+srun --ntasks=100 --cpu-bind=rank --distribution=block $ior -v -a MPIIO -i5 -g -w -r -e -C -b 1g -t 1m -s 1
+
+# Test 3 - HDF5 Streaming, 100 GB single-segment, single file
+# Small 100 MB 10-rank test
+#srun --ntasks=10 --cpu-bind=rank,verbose --distribution=block ior -v -a HDF5 -i1 -g -w -r -C -b 10m -t 1m -s 1
+echo "Test 3 - HDF5 Streaming, 100 GB single-segment, single file"
+srun --ntasks=100 --cpu-bind=rank --distribution=block $ior -v -a HDF5 -i5 -g -w -r -C -b 1g -t 1m -s 1
+
+# Test 4 - HDF5 Streaming, 100 GB ten-segment, single file
+# Small 100 MB 10-rank test
+#srun --ntasks=10 --cpu-bind=rank,verbose --distribution=block ior -v -a HDF5 -i1 -g -w -r -e -C -b 1m -t 1m -s 10
+echo "Test 4 - HDF5 Streaming, 100 GB ten-segment, single file"
+srun --ntasks=100 --cpu-bind=rank --distribution=block $ior -v -a HDF5 -i5 -g -w -r -e -C -b 10m -t 10m -s 10
+
+# Test 5 - POSIX Random, 1 GB single-segment, single file
+# Small 10 MB 10-rank test
+#srun --ntasks=10 --cpu-bind=rank,verbose --distribution=block ior -v -a POSIX -i1 -g -w -r -e -z -b 1m -t 2k -s 1
+echo "Test 5 - POSIX Random, 1 GB single-segment, single file"
+srun --ntasks=100 --cpu-bind=rank --distribution=block $ior -v -a POSIX -i5 -g -w -r -e -z -b 10m -t 2k -s 1
+
+# Test 6 - HDF5 Small Transfer, 1 GB ten-segment, single file
+# Small 10 MB 10-rank test
+#srun --ntasks=10 --cpu-bind=rank,verbose --distribution=block ior -v -a HDF5 -i1 -g -w -r -e -b 1m -t 2k -s 1
+echo "Test 6 - HDF5 Small Transfer, 1 GB ten-segment, single file"
+srun --ntasks=100 --cpu-bind=rank --distribution=block $ior -v -a HDF5 -i1 -g -w -r -e -b 10m -t 2k -s 1
