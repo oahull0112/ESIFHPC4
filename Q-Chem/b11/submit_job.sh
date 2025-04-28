3947a58809899056a61a845f3d908206e1a243ec
Hai Long <hlong@kl2.head.cm.kestrel.hpc.nrel.gov>
2025-04-28 12:56:13 -0600

    Update q-chem script with results

---
+++ Q-Chem/b11/submit_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+job_name=b11
+for job_type in small medium large; do
+ mkdir $job_type
+ cd $job_type
+ cp ../../input.com .
+ cp ../job.slurm .
+ sed -i "s/brianqc_test/$job_name.$job_type/" job.slurm
+ if [ "$job_type" == "small" ]; then
+  sed -i "s/6-311++G(2d,2p)/6-31G*/" input.com 
+ fi
+ if [ "$job_type" == "large" ]; then
+  sed -i "s/6-311++G(2d,2p)/aug-cc-pvtz/" input.com
+ fi
+ sbatch job.slurm
+ cd ..
+done
---
+++ Q-Chem/get_result.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+job_name=$1
+cd $job_name
+for job_type in small medium large; do
+ cd $job_type
+ echo $job_type
+ grep "SCF   energy" input.out | tail -1
+ grep "Total job time" input.out
+ cd ..
+done
