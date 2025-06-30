d7cfd9c7b984a92b58a5784d77474912ff87aebd
Kinshuk <kinshuk.panda@nrel.gov>
2025-06-30 10:04:21 -0600

    added extreme solar texas benchmark.

---
+++ Sienna-Ops/benchmarks/medium/run_extreme_solar_texas_opf.jl
@@ -0,0 +1,108 @@
+using PowerSystems
+# using PowerGraphics
+using PowerSimulations
+using PowerNetworkMatrices
+using Dates
+using CSV
+using HydroPowerSimulations
+using DataFrames
+using Logging
+using TimeSeries
+using StorageSystemsSimulations
+#using HiGHS #solver
+
+using Xpress
+#using PowerGrap
+
+const PSY = PowerSystems
+
+system_path = joinpath(@__DIR__, "input_data", "extreme_solar_texas", "final_sys_DA.json")
+sys_DA = PSY.System(system_path)
+
+mip_gap = 0.1
+
+optimizer = optimizer_with_attributes(
+                Xpress.Optimizer,
+                #"parallel" => "on",
+                "MIPRELSTOP" => mip_gap)
+
+
+logger= configure_logging(console_level=Logging.Info)
+
+template_uc = template_unit_commitment(;
+network = NetworkModel(CopperPlatePowerModel; use_slacks = true))
+set_device_model!(template_uc, HydroDispatch, HydroDispatchRunOfRiver)
+set_device_model!(template_uc, ThermalStandard, ThermalBasicUnitCommitment)
+set_device_model!(template_uc, ThermalMultiStart, ThermalBasicUnitCommitment)
+set_device_model!(template_uc, RenewableDispatch, RenewableFullDispatch,) 
+set_device_model!(template_uc, PowerLoad, StaticPowerLoad)
+set_device_model!(template_uc, DeviceModel(Line, 
+                                        StaticBranch;
+                                        use_slacks =true))
+
+set_device_model!(template_uc, DeviceModel(Transformer2W, 
+                                        StaticBranch;
+                                        use_slacks =true))  
+                                                             
+# set_device_model!(template_uc, AreaInterchange, StaticBranch)
+
+storage_model = DeviceModel(
+    EnergyReservoirStorage,
+    StorageDispatchWithReserves;
+    attributes=Dict(
+        "reservation" => true,
+        "energy_target" => false,
+        "cycling_limits" => false,
+        "regularization" => true,
+    ),
+)
+set_device_model!(template_uc, storage_model)
+
+# set_service_model!(template_uc, ServiceModel(VariableReserve{ReserveUp}, RangeReserve))
+# set_service_model!(template_uc, ServiceModel(VariableReserve{ReserveDown}, RangeReserve))
+initial_date = "2018-03-15"
+start_time = DateTime(string(initial_date,"T00:00:00"))
+model = DecisionModel(template_uc, sys_DA; name = "UC", optimizer = optimizer, horizon = Hour(24), calculate_conflict = true)
+models = SimulationModels(; decision_models = [model])
+
+steps_sim    = 2
+current_date = string( today() )
+sequence = SimulationSequence(
+    models = models,
+    # ini_cond_chronology = InterProblemChronology(),
+)
+
+sim = Simulation(
+    name = current_date * "_DR-test" * "_" * string(steps_sim)* "steps",
+    steps = steps_sim,
+    models = models,
+    initial_time = DateTime(string(initial_date,"T00:00:00")),
+    sequence = sequence,
+    simulation_folder = "EST",
+)
+
+build!(sim)
+
+execute!(sim)
+
+############################# RESULTS############################
+results = SimulationResults(sim)
+uc = get_decision_problem_results(results, "UC")
+
+vre_power =read_realized_variable(uc, "ActivePowerVariable__RenewableDispatch")
+thermal_power = read_realized_variable(uc, "ActivePowerVariable__ThermalMultiStart")
+thermals_power = read_realized_variable(uc, "ActivePowerVariable__ThermalStandard")
+
+solar = get_component(RenewableDispatch, sys_DA, "Angelina Solar")
+ts = get_time_series_array(Deterministic, solar, "max_active_power")
+
+# CSV.write("VRE_Power_Sim.csv", vre_power)
+# CSV.write("ThermalMulitStart_Power_Sim.csv", thermal_power)
+
+
+# lmp = read_realized_duals(uc)
+# uc_LMP_data = lmp["AreaParticipationAssignmentConstraint__ACBus"]
+
+# CSV.write("area_lmp.csv", uc_LMP_data)
+
+plot_dataframe(vre_power)
